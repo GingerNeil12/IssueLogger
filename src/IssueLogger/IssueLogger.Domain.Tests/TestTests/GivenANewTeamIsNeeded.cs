ecc60aff9ace9080be8a4643e3d313ea238ef85a
Neil Earlam <neil.earlam2019@outlook.com>
2021-09-21 23:35:17 +0100

    Added the tests for constructing a new Team

---
+++ src/IssueLogger/IssueLogger.Domain.Tests/TestTests/GivenANewTeamIsNeeded.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using IssueLogger.Domain.Common;
+using IssueLogger.Domain.Models;
+using IssueLogger.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IssueLogger.Domain.Tests.TestTests
+{
+    [TestClass]
+    [TestCategory(TestCategories.Unit)]
+    public class GivenANewTeamIsNeeded
+    {
+        [TestMethod]
+        public void WhenConstructedWithValidProperties_ThenShouldNotBeNull()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var code = "Code";
+            var name = "Name";
+
+            // Act
+            var teamUnderTest = new Team(id, code, name);
+
+            // Assert
+            teamUnderTest.Should().NotBeNull();
+            teamUnderTest.Id.Should().Be(id);
+            teamUnderTest.Code.Should().Be(code);
+            teamUnderTest.Name.Should().Be(name);
+            teamUnderTest.NormalizedCode.Should().Be(code.Normalize());
+            teamUnderTest.NormalizedCode.IsNormalized().Should().BeTrue();
+            teamUnderTest.NormalizedName.Should().Be(name.Normalize());
+            teamUnderTest.NormalizedName.IsNormalized().Should().BeTrue();
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(GetData), DynamicDataSourceType.Method)]
+        public void WhenConstructedWithInvalidProperties_ThenThrowsArgumentNullException
+        (
+            string paramName,
+            string code,
+            string name,
+            string errorMessage
+        )
+        {
+            // Arrange
+            // Act
+            Action action = () => new Team(Guid.NewGuid(), code, name);
+
+            // Assert
+            action.Should().Throw<ArgumentNullException>()
+                .WithParameterName(paramName)
+                .WithMessage(errorMessage);
+        }
+
+        private static IEnumerable<object[]> GetData()
+        {
+            var code = "Code";
+            var name = "Name";
+
+            yield return new object[] { nameof(code), code, name, $"{Resources.PropertyNullOrBlank} (Parameter '{nameof(code)}')" };
+            yield return new object[] { nameof(code), null, name, $"{Resources.PropertyNullOrBlank} (Parameter '{nameof(code)}')" };
+            yield return new object[] { nameof(name), code, string.Empty, $"{Resources.PropertyNullOrBlank} (Parameter '{nameof(name)}')" };
+            yield return new object[] { nameof(name), code, null, $"{Resources.PropertyNullOrBlank} (Parameter '{nameof(name)}')" };
+        }
+    }
+}
--- src/IssueLogger/IssueLogger.Domain/Common/Resources.Designer.cs
+++ src/IssueLogger/IssueLogger.Domain/Common/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace IssueLogger.Domain.Common {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace IssueLogger.Domain.Common {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("IssueLogger.Domain.Common.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace IssueLogger.Domain.Common {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace IssueLogger.Domain.Common {
         /// <summary>
         ///   Looks up a localized string similar to Property cannot be null or blank..
         /// </summary>
-        internal static string PropertyNullOrBlank {
+        public static string PropertyNullOrBlank {
             get {
                 return ResourceManager.GetString("PropertyNullOrBlank", resourceCulture);
             }
