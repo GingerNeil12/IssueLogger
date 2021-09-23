306f6242830d85a9b1712e6b57cfc50473fda591
Neil Earlam <neil.earlam@sms-plc.com>
2021-09-23 17:02:51 +0100

    Added the Member.Actions file

---
+++ src/IssueLogger/IssueLogger.Domain/Models/Actions/Member.Actions.cs
@@ -0,0 +1,26 @@
+﻿using IssueLogger.Domain.Models.ValueObjects;
+using System;
+
+namespace IssueLogger.Domain.Models
+{
+    public partial class Member
+    {
+        public void Block()
+        {
+            BlockedStatus = new BlockedStatus
+            {
+                IsBlocked = true,
+                BlockedOn = DateTime.Now
+            };
+        }
+
+        public void Unblock()
+        {
+            BlockedStatus = new BlockedStatus
+            {
+                IsBlocked = false,
+                BlockedOn = new DateTime()
+            };
+        }
+    }
+}
