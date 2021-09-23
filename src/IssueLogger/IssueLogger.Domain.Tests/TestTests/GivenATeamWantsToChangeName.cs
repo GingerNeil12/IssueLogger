199ac4da92b6e9b7c48d59147cc5c730ab9e046b
Neil Earlam <neil.earlam@sms-plc.com>
2021-09-23 15:17:27 +0100

    Removed unused usings

--- src/IssueLogger/IssueLogger.Domain.Tests/TestTests/GivenATeamWantsToChangeName.cs
+++ src/IssueLogger/IssueLogger.Domain.Tests/TestTests/GivenATeamWantsToChangeName.cs
@@ -4,10 +4,6 @@ using IssueLogger.Domain.Models;
 using IssueLogger.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IssueLogger.Domain.Tests.TestTests
 {
