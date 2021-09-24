49f60bf2d955975583f8e0c6f9b2bc25ced31c9c
Neil Earlam <neil.earlam@sms-plc.com>
2021-09-24 16:19:19 +0100

    Added tests to check that the Block and Unblock methods on the Member work as intended

---
+++ src/IssueLogger/IssueLogger.Domain.Tests/MemberTests/GivenAMemberIsBlocked.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using IssueLogger.Domain.Models;
+using IssueLogger.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace IssueLogger.Domain.Tests.MemberTests
+{
+    [TestClass]
+    [TestCategory(TestCategories.Unit)]
+    public class GivenAMemberIsBlocked
+    {
+        [TestMethod]
+        public void WhenUnblockIsCalled_ThenTheirBlockedStatusShouldShowThemUnblocked()
+        {
+            // Arrange
+            var memberUnderTest = Member.Create("USER_ID", Guid.NewGuid());
+
+            // Act
+            memberUnderTest.Unblock();
+
+            // Assert
+            memberUnderTest.BlockedStatus.Should().NotBeNull();
+            memberUnderTest.BlockedStatus.IsBlocked.Should().BeFalse();
+            memberUnderTest.BlockedStatus.BlockedOn.Should().Be(new DateTime());
+        }
+    }
+}
---
+++ src/IssueLogger/IssueLogger.Domain.Tests/MemberTests/GivenAMemberIsUnblocked.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using IssueLogger.Domain.Models;
+using IssueLogger.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace IssueLogger.Domain.Tests.MemberTests
+{
+    [TestClass]
+    [TestCategory(TestCategories.Unit)]
+    public class GivenAMemberIsUnblocked
+    {
+        [TestMethod]
+        public void WhenBlockIsCalled_ThenTheMembersBlockedStatusShouldUpdateToShowBlocked()
+        {
+            // Arrange
+            var memberUnderTest = CreateMember();
+
+            // Act
+            memberUnderTest.Block();
+
+            // Assert
+            memberUnderTest.BlockedStatus.Should().NotBeNull();
+            memberUnderTest.BlockedStatus.IsBlocked.Should().BeTrue();
+            memberUnderTest.BlockedStatus.BlockedOn.Should().BeOnOrBefore(DateTime.Now);
+        }
+
+        private static Member CreateMember()
+        {
+            return Member.Create("USER_ID", Guid.NewGuid());
+        }
+    }
+}
