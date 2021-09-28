6cc0a0fcf5f12db691335454029e14fc7380e817
Neil Earlam <neil.earlam@sms-plc.com>
2021-09-28 15:55:21 +0100

    Added testing for Adding and Removing roles from a member

---
+++ src/IssueLogger/IssueLogger.Domain.Tests/MemberTests/GivenAMemberIsToBeRemovedFromARole.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using IssueLogger.Domain.Models;
+using IssueLogger.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace IssueLogger.Domain.Tests.MemberTests
+{
+    [TestClass]
+    [TestCategory(TestCategories.Unit)]
+    public class GivenAMemberIsToBeRemovedFromARole
+    {
+        [TestMethod]
+        public void WhenTheyHaveThatRole_ThenItShouldBeRemoved()
+        {
+            // Arrange
+            var memberUnderTest = CreateMember();
+            var roleIdOne = Guid.NewGuid().ToString();
+            var roleIdTwo = Guid.NewGuid().ToString();
+            memberUnderTest.AddRole(roleIdOne);
+            memberUnderTest.AddRole(roleIdTwo);
+
+            // Act
+            memberUnderTest.RemoveRole(roleIdOne);
+
+            // Assert
+            memberUnderTest.Roles.Should().NotBeNull();
+            memberUnderTest.Roles.Count(role => role.RoleId == roleIdOne).Should().Be(0);
+            memberUnderTest.Roles.Should().HaveCount(1);
+            memberUnderTest.Roles.First().RoleId.Should().Be(roleIdTwo);
+        }
+
+        [TestMethod]
+        public void WhenTheyDoNotHaveThatRole_ThenTheirRolesShouldBeUnaffected()
+        {
+            // Arrange
+            var memberUnderTest = CreateMember();
+            var roleId = Guid.NewGuid().ToString();
+            memberUnderTest.AddRole(roleId);
+
+            // Act
+            memberUnderTest.RemoveRole(Guid.NewGuid().ToString());
+
+            // Assert
+            memberUnderTest.Roles.Should().NotBeNull();
+            memberUnderTest.Roles.Count(role => role.RoleId == roleId).Should().Be(1);
+            memberUnderTest.Roles.First().RoleId.Should().Be(roleId);
+        }
+
+        private static Member CreateMember()
+        {
+            return Member.Create("USER_ID", Guid.NewGuid());
+        }
+    }
+}
---
+++ src/IssueLogger/IssueLogger.Domain.Tests/MemberTests/GivenARoleIsToBeAddedToAMember.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using IssueLogger.Domain.Models;
+using IssueLogger.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace IssueLogger.Domain.Tests.MemberTests
+{
+    [TestClass]
+    [TestCategory(TestCategories.Unit)]
+    public class GivenARoleIsToBeAddedToAMember
+    {
+        [TestMethod]
+        public void WhenTheyAreNotAlreadyInThatRole_ThenTheRoleShouldBeAddedToThem()
+        {
+            // Arrange
+            var memberUnderTest = CreateMember();
+            var roleId = Guid.NewGuid().ToString();
+
+            // Act
+            memberUnderTest.AddRole(roleId);
+
+            // Assert
+            memberUnderTest.Roles.Should().NotBeNull();
+            memberUnderTest.Roles.Should().HaveCount(1);
+            memberUnderTest.Roles.First().RoleId.Should().Be(roleId);
+            memberUnderTest.Roles.First().UserId.Should().Be(memberUnderTest.UserId);
+            memberUnderTest.Roles.First().TeamId.Should().Be(memberUnderTest.TeamId);
+            memberUnderTest.Roles.First().Addedon.Should().BeOnOrBefore(DateTime.Now);
+        }
+
+        [TestMethod]
+        public void WhenTheyAreAlreadyInTheRole_ThenTheRoleShouldNotBeAddedTwice()
+        {
+            // Arrange
+            var memberUnderTest = CreateMember();
+            var roleId = Guid.NewGuid().ToString();
+
+            // Act
+            memberUnderTest.AddRole(roleId);
+            memberUnderTest.AddRole(roleId);
+
+            // Assert
+            memberUnderTest.Roles.Should().NotBeNull();
+            memberUnderTest.Roles.Count(role => role.RoleId == roleId).Should().Be(1);
+            memberUnderTest.Roles.First().RoleId.Should().Be(roleId);
+            memberUnderTest.Roles.First().UserId.Should().Be(memberUnderTest.UserId);
+            memberUnderTest.Roles.First().TeamId.Should().Be(memberUnderTest.TeamId);
+            memberUnderTest.Roles.First().Addedon.Should().BeOnOrBefore(DateTime.Now);
+        }
+
+        private static Member CreateMember()
+        {
+            return Member.Create("USER_ID", Guid.NewGuid());
+        }
+    }
+}
