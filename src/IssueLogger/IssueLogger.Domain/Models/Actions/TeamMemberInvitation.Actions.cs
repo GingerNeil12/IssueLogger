8d752524ee488ea3b07f75f51b9f1b7011bfa799
Neil Earlam <neil.earlam@sms-plc.com>
2021-09-28 17:07:03 +0100

    Added the actions available for the team member invitation

---
+++ src/IssueLogger/IssueLogger.Domain/Models/Actions/TeamMemberInvitation.Actions.cs
@@ -0,0 +1,31 @@
+﻿using IssueLogger.Domain.Enums;
+
+namespace IssueLogger.Domain.Models
+{
+    public partial class TeamMemberInvitation
+    {
+        public void Accept()
+        {
+            if (Status == TeamMemberInvitationStatus.Pending)
+            {
+                Status = TeamMemberInvitationStatus.Accepted;
+            }
+        }
+
+        public void Decline()
+        {
+            if (Status == TeamMemberInvitationStatus.Pending)
+            {
+                Status = TeamMemberInvitationStatus.Declined;
+            }
+        }
+
+        public void Revoke()
+        {
+            if (Status == TeamMemberInvitationStatus.Pending)
+            {
+                Status = TeamMemberInvitationStatus.Revoked;
+            }
+        }
+    }
+}
